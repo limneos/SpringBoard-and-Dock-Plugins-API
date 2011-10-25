182c4827b2c3859493f92f270b84ff8a3738fc28
Elias Limneos <iphone@limneos.net>
2011-10-25 17:19:00 +0300

    Update PMSBPlugins.h

--- PMSBPlugins.h
+++ PMSBPlugins.h
@@ -10,6 +10,9 @@
 
 @interface PMSBPlugins : NSObject 
 +(PMSBPlugins *)sharedInstance;
+-(NSString *)pluginOnCurrentPage;
+-(NSString *)pluginNameOnPage:(int)page;
 -(void)scrollToPluginNamed:(NSString *)pluginName;
+-(void)scrollToPluginNamed:(NSString *)pluginName animated:(BOOL)animated;
 @end
 
